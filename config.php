479a735aae7360007ef227886b604b18baaaa84f
HDDen <62592944+HDDen@users.noreply.github.com>
2022-03-19 23:41:42 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,20 +1,17 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'https://'.$_SERVER['HTTP_HOST'].'/admin/');
-define('HTTP_CATALOG', 'https://'.$_SERVER['HTTP_HOST'].'/');
+define('HTTP_SERVER', 'https://'.$_SERVER['HTTP_HOST'].'/');
 
 // HTTPS
-define('HTTPS_SERVER', 'https://'.$_SERVER['HTTP_HOST'].'/admin/');
-define('HTTPS_CATALOG', 'https://'.$_SERVER['HTTP_HOST'].'/');
+define('HTTPS_SERVER', 'https://'.$_SERVER['HTTP_HOST'].'/');
 
 // DIR
-define('DIR_APPLICATION', rtrim($_SERVER['DOCUMENT_ROOT'], '\/').'/admin/');
+define('DIR_APPLICATION', rtrim($_SERVER['DOCUMENT_ROOT'], '\/').'/catalog/');
 define('DIR_SYSTEM', rtrim($_SERVER['DOCUMENT_ROOT'], '\/').'/system/');
 define('DIR_IMAGE', rtrim($_SERVER['DOCUMENT_ROOT'], '\/').'/image/');
 define('DIR_STORAGE', rtrim($_SERVER['DOCUMENT_ROOT'], '\/').'/../storage/');
-define('DIR_CATALOG', rtrim($_SERVER['DOCUMENT_ROOT'], '\/').'/catalog/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
-define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
 define('DIR_CACHE', DIR_STORAGE . 'cache/');
 define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
@@ -31,7 +28,3 @@ define('DB_PASSWORD', '4Y6s6P0p');
 define('DB_DATABASE', 'u1095413_re_shop');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
-
-// OpenCart API
-define('OPENCART_SERVER', 'https://www.opencart.com/');
-define('OPENCARTFORUM_SERVER', 'https://opencartforum.com/');
